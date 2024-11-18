459a27f37d480c876f4ba614a17cf5cd577415ba
stfx86 <mustaphabennasser8@gmail.com>
2024-11-18 22:47:26 +0100

    It

---
+++ src/bank_sys/classes.java
@@ -0,0 +1,20 @@
+package bank_sys;
+
+class H {
+    void hello(){
+        System.out.println("hello wols");
+    }
+
+    
+}
+/**
+ * classes
+ */
+ class HI {
+    void hello(){
+        System.out.println("hello to youssef");
+    }
+
+
+}
+
--- src/bank_sys/srcode.java
+++ src/bank_sys/srcode.java
@@ -2,5 +2,10 @@ package bank_sys;
 
 public class srcode {
 	public static void main(String[] kk) {
-		System.out.println("my name is zray9ammmmmmmmmpp");
+		//System.out.println("my name is zray9ammmmmmmmmpp");
+		H pp= new H();
+		// pp.hello();
+     HI oo =new HI();
+	 
+
 	}}
