fb5ec3db92d42306f8092454bd1cf2f4d562a67f
nj-ysf <najiy514@gmail.com>
2024-11-25 19:03:38 +0100

    25\11\2024 update

--- src/bank_sys/classes.java
+++ src/bank_sys/classes.java
@@ -1,20 +1,52 @@
 package bank_sys;
 
             
-class client {
-    String nom,prenom;
-    int code;
+class Client {
+     protected  String nom ;   // protected pour la securty .
+     protected String prenom;
+     protected int code;
+     protected String ville ;
+      public Client(){
+
+      }
+      public Client(String nom ,String Prenom ,int code ,String ville)
+      {this.nom =nom ;
+        this.prenom ;
+        this.code =code ;
+        this.ville =ville ;
+
+      }
+
     /*
      * methods to later 
      */
 
     
 }
+  classe Directeur 
+  {  
+    
+
+}
+  class Compte{
+    String type ;
+    int num ;
+    int code  ;
+    public Compte(String t ,int n ,int c){
+        this.type =t ;
+        this.num =n ;
+        ths.c = c ;
+    }
+
+  }
 /**
  * classes
  */ 
-class employe extends client {
-    ;;;;;;;;;;;;;
+  class employe extends client {
+          String ville ;
+      
+
+
 
 
     
